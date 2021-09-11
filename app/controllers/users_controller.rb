05dd0afd5ddf0f9b34a6028bb7372827b574f254
James Cassello III <senpaijames.cassello@gmail.com>
2021-09-10 22:50:20 -0600

    converted user json data to class data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,76 +1,31 @@
 class UsersController < ActionController::Base
+   require "user"
    def index
       userdata = {
          "users": [
-            {
-               "id": 1,
-               "first_name": "Jeanette",
-               "last_name": "Penddreth",
-               "email": "jpenddreth0@census.gov",
-               "gender": "Female",
-               "ip_address": "26.58.193.2"
-            }, {
-               "id": 2,
-               "first_name": "Giavani",
-               "last_name": "Frediani",
-               "email": "gfrediani1@senate.gov",
-               "gender": "Male",
-               "ip_address": "229.179.4.212"
-            }, {
-               "id": 3,
-               "first_name": "Noell",
-               "last_name": "Bea",
-               "email": "nbea2@imageshack.us",
-               "gender": "Female",
-               "ip_address": "180.66.162.255"
-            }, {
-               "id": 4,
-               "first_name": "Willard",
-               "last_name": "Valek",
-               "email": "wvalek3@vk.com",
-               "gender": "Male",
-               "ip_address": "67.76.188.26"
-            }
+            User.new('Jeanette', 'Penddreth', 1),
+            User.new('Giavani', 'Frediani', 2),
+            User.new('Noell', 'Bea', 3),
+            User.new('Willard', 'Valek', 4)
          ]
       }
-    # render plain: 'hello world'
-   render json: userdata
+    # rendering json on page: user data
+      render json: userdata
    end
+
    def show
+      jen = User.new('Jeanette', 'Penddreth', 1),
       userdata = {
          "users": [
-            {
-               "id": 1,
-               "first_name": "Jeanette",
-               "last_name": "Penddreth",
-               "email": "jpenddreth0@census.gov",
-               "gender": "Female",
-               "ip_address": "26.58.193.2"
-            }, {
-               "id": 2,
-               "first_name": "Giavani",
-               "last_name": "Frediani",
-               "email": "gfrediani1@senate.gov",
-               "gender": "Male",
-               "ip_address": "229.179.4.212"
-            }, {
-               "id": 3,
-               "first_name": "Noell",
-               "last_name": "Bea",
-               "email": "nbea2@imageshack.us",
-               "gender": "Female",
-               "ip_address": "180.66.162.255"
-            }, {
-               "id": 4,
-               "first_name": "Willard",
-               "last_name": "Valek",
-               "email": "wvalek3@vk.com",
-               "gender": "Male",
-               "ip_address": "67.76.188.26"
-            }
+            User.new('Jeanette', 'Penddreth', 1),
+            User.new('Giavani', 'Frediani', 2),
+            User.new('Noell', 'Bea', 3),
+            User.new('james', 'cassello', 1),
+            User.new('Willard', 'Valek', 4)
          ]
       }
-   #  render plain: params[:id]
-   render json: userdata[:users].select {|user| user[:id] == params[:id].to_i}
+      #  render plain: params[:id]
+      render json: userdata[:users].select {|user| user.get_id() == params[:id].to_i}
+      # render json: user.User_Identification
    end
 end
