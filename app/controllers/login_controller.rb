6b44aa9a77d7e4486fda68c3ceeb950af3c78594
James Cassello III <senpaijames.cassello@gmail.com>
2021-12-25 00:42:59 -0700

    fixed syntax errors

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,12 +1,12 @@
 class LoginController < ActionController::Base
    
    def show
-      @page_title "Login page"
+      @page_title = "Login page"
       render :show
    end
 
    def login
-      @page_title "login successful"
+      @page_title = "login successful"
       return_users = User.where(first_name: params[:fname], last_name: params[:lname])
       render plain: "return users#{return_users[0]}"
    end
