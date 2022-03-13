0007d6cb3cc2896ff86ab1d8d0469d8bf79f8b83
James Cassello III <senpaijames.cassello@gmail.com>
2022-03-12 22:32:36 -0700

    updated redirect to dashboard controller  -redirect to /login -redirect to /dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
     @page_title = "Login page"
   end
@@ -8,7 +9,7 @@ class SessionsController < ApplicationController
   if user && user.authenticate(params[:password])
    session[:user_id] = user.id
    flash[:success] = 'Successfully Logged In'
-   redirect_to '/'
+   redirect_to '/dashboard'
   else
    flash[:warning] = 'Invalid Username or Password'
    redirect_to '/login'
@@ -18,6 +19,6 @@ class SessionsController < ApplicationController
  def destroy
    session[:user_id] = nil
    flash[:success] = 'Successfully Logged Out'
-   redirect_to '/'
+   redirect_to '/login'
   end
  end
