515fe9a64fb74b217cef880e613c06e3f4ae208e
James Cassello III <senpaijames.cassello@gmail.com>
2022-03-12 22:44:04 -0700

    added new get dashboard#index route, move routes inline to separate get//post

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'user#show'
   get '/register/',  to: 'register#index'
   get '/checkuser/', to: 'user#validate'
-  post '/register/', to: 'register#createuser'
   get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  get '/', to: 'home#index' 
-  # get {url address}, to: {controller name}#{controller function}
+  get '/', to: 'home#index'
+  get '/dashboard', to: 'dashboard#index'
+
+  post '/register/', to: 'register#createuser'  
+  post '/login', to: 'sessions#create'
   
+  # get {url address}, to: {controller name}#{controller function}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
