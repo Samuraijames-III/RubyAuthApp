0ae46c11e3bedff18231723011f9ac63374a90dd
James Cassello III <senpaijames.cassello@gmail.com>
2022-09-08 17:45:03 -0600

    updated to better compare password fields -updated IF's to check if user pwd is the same during pwd change -updated IF's to check if user pwd, new pwd is the same during update -updated IF's to check if original pwd is the same during password update
    changed header tag msg during password change success

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -3,46 +3,35 @@ require 'bcrypt'
 class PasswordsController < ApplicationController
     include BCrypt
 
-    def passwordchanged
-    end
-
     def update
         user = current_user;
-
-        #user entered correct password
-        if user.authenticate(params[:password])
-            helpers.flash_message :danger, "current password doesnt match"
-            redirect_to '/dashboard'
-        end
         
-        puts "#{user.authenticate(params[:password])}"
-        puts "is this working #{params[:new_password]}" 
-        puts "is this also working #{Password.new(user.password)}" 
         
         #if new password is the same as new password, doesnt not save
-        if Password.new(user.password) == params[:new_password]
+        if  Password.new(current_user.password) == params[:new_password]
             helpers.flash_message :danger, "new password can't match current password, please try again"
-            redirect_to '/login'
-        end
-        puts "#{redirect_to '/login'}"
-
-        #if new password doesnt match confirm password
-        if params[:new_password] != params[:new_password_confirm]
-            helpers.flash_message :danger, "password, password confirm needs to match"
             redirect_to '/dashboard'
         end
-
-        puts "#{params[:new_password] != params[:new_password_confirm]}"
-
         
-        #create a new password and save the user ID
-        user.password = params[:new_password] 
-        user.save
+        #if new password doesnt match confirm password
+        if  params[:new_password] != params[:new_password_confirm]
+            helpers.flash_message :danger, "password, password confirm needs to match, please try again"
+            redirect_to '/dashboard' 
+        end
         
-        helpers.flash_message :success, "change successful, logout for new session"
-        redirect_to '/passwordchangesuccess'
+        #trying to update password, with existing password incorrect in first field.
+        if  current_user.password != Password.new(current_user.password)
+            helpers.flash_message :danger, "existing password different from original, please try again"
+            redirect_to '/dashboard' && return
+        end
+        
+
     end
 end
+    
+        #  if  @user.password = params[:new_password_cofirm]
+        #      @user.save!
+        #      redirect_to '/passwordchangesuccess'
+        #  end
 
-
-# you need to fix the double redirect error, by checking how rails is redirecting the error message!
+        # you need to fix the double redirect error, by checking how rails is redirecting the error message!
